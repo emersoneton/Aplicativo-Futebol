8da235dcedc91a8a9e3a88c2415bf38dc9fd641a
lucianofigueiro <lucianofigueiro37@gmail.com>
2019-10-04 23:57:08 -0300

    Desfazendo o commit de alteração errada

---
+++ app/src/main/java/com/bruno/futbol/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.bruno.futbol;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mostrarMainActivity();
+            }
+        }, 2000);
+    }
+
+    private void mostrarMainActivity() {
+        Intent intent = new Intent(
+                SplashActivity.this, com.bruno.futbol.Formulario_JogadorActivity.class
+        );
+        startActivity(intent);
+        finish();
+    }
+}
